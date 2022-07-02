3200e221a1f0abb0580281aa24fa2926467e295b
Vuduong96 <83565724+Vuduong96@users.noreply.github.com>
2022-07-02 14:41:53 +1000

    Database and migrations

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author':'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'July 1, 2022'
-
-    },
-    {
-        'author':'Natcha',
-        'title': 'Blog Post 2',
-        'content': 'First post content',
-        'date_posted': 'July 1, 2022'
-
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts # key string need to the same spelling as the one in 'home.html' 
+        'posts': Post.objects.all() # key string need to the same spelling as the one in 'home.html' 
     }
     return render(request, 'blog/home.html', context)
 
